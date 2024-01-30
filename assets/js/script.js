3ca516ea6463e12b8f58fa49c44b0ae95a35149b
Lowell Hallassou <lo@Lowells-MacBook-Air.local>
2024-01-30 15:41:09 +0000

    Adding the current day forecast to webpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,16 +17,141 @@ fetch(queryURL)
         console.log(data)
 
         let cityName = data.name;
-        let weather = data.weather.main;
+        let weatherIcon = data.weather.icon;
         let temp = data.main.temp;
         let windSpeed = data.wind.speed;
         let humidity = data.main.humidity;
 
+
         let todayFC = document.querySelector('#today');
+
+        const forecastTitle = function (cityName) {
+
         let todayH1 = document.createElement('h1');
         todayH1.textContent = cityName;
         todayFC.append(todayH1);
+        }
 
+        forecastTitle(cityName);
+
+
+        let todayTemp = document.createElement('p');
+        let convertedTemp = temp - 273.15;
+
+        todayTemp.textContent = "Temp: " + convertedTemp.toFixed(2) + "°C"
+        todayFC.append(todayTemp);
+
+
+        let todayWS = document.createElement('p');
+        todayWS.textContent = 'Wind: ' + windSpeed.toFixed(1) + ' KPH'
+        todayFC.append(todayWS);
+
+        let todayHumidity = document.createElement('p');
+        todayHumidity.textContent = 'Humidity: ' + humidity + '%';
+        todayFC.append(todayHumidity);
+
+        const weather = function (weatherIcon) {
+        let icon = document.createElement('img');
+        switch (weatherIcon) {
+            case '01d':
+                icon.setAttribute('src', weatherIconURL);
+                icon.setAttribute('alt', 'weather icon');
+                todayFC.append(icon);
+                break
+            case '02d':
+                icon.setAttribute('src', weatherIconURL);
+                icon.setAttribute('alt', 'weather icon');
+                todayFC.append(icon);
+                break
+            case '03d':
+                icon.setAttribute('src', weatherIconURL);
+                icon.setAttribute('alt', 'weather icon');
+                todayFC.append(icon);
+                break
+            case '04d':
+                icon.setAttribute('src', weatherIconURL);
+                icon.setAttribute('alt', 'weather icon');
+                todayFC.append(icon);
+                break
+            case '09d':
+                icon.setAttribute('src', weatherIconURL);
+                icon.setAttribute('alt', 'weather icon');
+                todayFC.append(icon);
+                break
+            case '10d':
+                icon.setAttribute('src', weatherIconURL);
+                icon.setAttribute('alt', 'weather icon');
+                todayFC.append(icon);
+                break
+            case '11d':
+                icon.setAttribute('src', weatherIconURL);
+                icon.setAttribute('alt', 'weather icon');
+                todayFC.append(icon);
+                break;
+            case '13d':
+                icon.setAttribute('src', weatherIconURL);
+                icon.setAttribute('alt', 'weather icon');
+                todayFC.append(icon);
+                break
+            case '50d':
+                icon.setAttribute('src', weatherIconURL);
+                icon.setAttribute('alt', 'weather icon');
+                todayFC.append(icon);
+                break
+            case '01n':
+                icon.setAttribute('src', weatherIconURL);
+                icon.setAttribute('alt', 'weather icon');
+                todayFC.append(icon);
+                break
+            case '02n':
+                icon.setAttribute('src', weatherIconURL);
+                icon.setAttribute('alt', 'weather icon');
+                todayFC.append(icon);
+                break
+            case '03n':
+                icon.setAttribute('src', weatherIconURL);
+                icon.setAttribute('alt', 'weather icon');
+                todayFC.append(icon);
+                break
+            case '04n':
+                icon.setAttribute('src', weatherIconURL);
+                icon.setAttribute('alt', 'weather icon');
+                todayFC.append(icon);
+                break
+            case '09n':
+                icon.setAttribute('src', weatherIconURL);
+                icon.setAttribute('alt', 'weather icon');
+                todayFC.append(icon);
+                break
+            case '10n':
+                icon.setAttribute('src', weatherIconURL);
+                icon.setAttribute('alt', 'weather icon');
+                todayFC.append(icon);
+                break
+            case '11n':
+                icon.setAttribute('src', weatherIconURL);
+                icon.setAttribute('alt', 'weather icon');
+                todayFC.append(icon);
+                break;
+            case '13n':
+                icon.setAttribute('src', weatherIconURL);
+                icon.setAttribute('alt', 'weather icon');
+                todayFC.append(icon);
+                break
+            case '50n':
+                icon.setAttribute('src', weatherIconURL);
+                icon.setAttribute('alt', 'weather icon');
+                todayFC.append(icon);
+                break
+
+        }
+        }
+
+        weather(weatherIcon);
+    
+
+       
+        
     })
 
 
